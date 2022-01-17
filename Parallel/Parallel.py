0bd41405778526f356729699242cc0ef8376d976
Aravindhan <aravindganpath@gmail.com>
2022-01-17 10:04:46 +0530

    Update Parallel.py

--- Parallel/Parallel.py
+++ Parallel/Parallel.py
@@ -12,8 +12,8 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 drawing_styles = mp.solutions.drawing_styles
 
-mod = pickle.load(open('', 'rb')) # Location of XGBoost.sav
-le = pickle.load(open('', 'rb')) # Location of XEncoder1.sav
+mod = pickle.load(open('', 'rb')) #Location of XGBoost.sav
+le = pickle.load(open('', 'rb')) #Location of XEncoder1.sav
 word = ' '
 
 @ray.remote(num_cpus = 0.5)
